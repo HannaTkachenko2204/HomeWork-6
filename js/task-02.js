59fd114a031d3d4db2db9794f511f7d708be2755
HannaTkachenko2204 <anna.kuvakina91@gmail.com>
2024-01-27 17:25:59 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -9,12 +9,24 @@ const ingredients = [
 
 const listOfingredients = document.querySelector('#ingredients');
 
-const contextOfList = ingredients.forEach(ingredient => 
-  {
-    const elementOfList = document.createElement('li');
-    elementOfList.textContent = ingredient;
-    elementOfList.classList.add('item');
-    console.log(elementOfList);
-  });
+//ForEach не подходит, если нужно вставить все элементы списка одной опирацией
 
-  listOfingredients.append(contextOfList);
+// ingredients.forEach(ingredient => 
+//   {
+//     const elementOfList = document.createElement('li');
+//     elementOfList.textContent = ingredient;
+//     elementOfList.classList.add('item');
+//     console.log(elementOfList);
+//     listOfingredients.append(elementOfList);
+//   });
+
+
+const contextOfList = ingredients.map(ingredient => {
+  const elementOfList = document.createElement('li');
+  elementOfList.textContent = ingredient;
+  elementOfList.classList.add('item');
+  console.log(elementOfList);
+  return elementOfList;
+});
+
+listOfingredients.append(...contextOfList);
