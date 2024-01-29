dbb47ae2be1ab665d2e35d98d172f0ce9f6e4f89
HannaTkachenko2204 <anna.kuvakina91@gmail.com>
2024-01-29 15:26:21 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,7 +1,13 @@
 let counterValue = 0;
+//let step = 1; (создаем переменную для увеличения на любой шаг 'counterValue-=step')
+
 const btnDecrement = document.querySelector('button[data-action="decrement"]');
 const btnIncrement = document.querySelector('button[data-action="increment"]');
 const valueEl = document.querySelector('#value');
+
+// const counterEl = document.querySelector('#counter');
+// counterEl.firstElementChild.addEventListener('click', clickDecrement);
+// counterEl.lastElementChild.addEventListener('click', clickIncrement);
 
 const clickDecrement = event => {
     console.log("BtnDecrement was clicked");
