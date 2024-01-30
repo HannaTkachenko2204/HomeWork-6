c2f4661c3e4e24872e821d64603dca31625131d3
HannaTkachenko2204 <anna.kuvakina91@gmail.com>
2024-01-30 20:23:32 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,31 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const boxesEl = document.querySelector('#boxes');
+const inputEl = document.querySelector('.input');
+const btnCreate = document.querySelector('button[data-create]');
+const btnDestroy = document.querySelector('button[data-destroy]');
+
+// inputEl.addEventListener('input', (event) => {
+//   const amount = event.currentTarget.value;
+//   console.log(amount);
+// })
+
+
+
+function createBoxes(amount) {
+   for (let i = 1; i <= amount; i+=1) {
+    const divEl = document.createElement('div');
+    console.log(divEl);
+    boxesEl.append(divEl);
+    console.log(boxesEl);
+   }
+}
+
+function destroyBoxes() {
+  boxesEl.reset();
+}
+
+btnCreate.addEventListener('click', createBoxes);
+//btnDestroy.addEventListener('click', destroyBoxes);
