89e66f1c58f377c2cbd832d4b6226d4c940d4653
HannaTkachenko2204 <anna.kuvakina91@gmail.com>
2024-01-30 14:56:52 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,1 +1,13 @@
-const input = document.querySelector("#validation-input");
+const inputEl = document.querySelector("#font-size-control");
+const textEl = document.querySelector("#text");
+
+//console.log(inputEl.value);
+textEl.style.fontSize = `${inputEl.value}px`;
+
+inputEl.addEventListener('input', (event) => {
+    const currentSize = event.currentTarget.value;
+    console.log(currentSize);
+    textEl.style.fontSize = `${currentSize}px`;
+    //console.log(textEl.style.fontSize);
+})
+
