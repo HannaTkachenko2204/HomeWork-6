df6c24298592d0cc26056008879b6e9aa0ee8124
HannaTkachenko2204 <anna.kuvakina91@gmail.com>
2024-01-30 17:27:00 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const objectOfData = {};
+    const form = event.target;
+    const email = form.elements.email.value;
+    objectOfData.email = email;
+    const password = form.elements.password.value;
+    objectOfData.password = password;
+    console.log(objectOfData);
+    if(email === '' || password === '') {
+        alert('All fields must be filled in!');
+    }
+    //console.log(`${email}, ${password}`);
+    form.reset();
+}
