4daed86858b8421e019bfd2238189354b8ddb6ff
HannaTkachenko2204 <anna.kuvakina91@gmail.com>
2024-01-30 21:11:11 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -5,28 +5,23 @@ function getRandomHexColor() {
 }
 
 const boxesEl = document.querySelector('#boxes');
-const inputEl = document.querySelector('.input');
+const inputEl = document.querySelector('input');
 const btnCreate = document.querySelector('button[data-create]');
 const btnDestroy = document.querySelector('button[data-destroy]');
-
-// inputEl.addEventListener('input', (event) => {
-//   const amount = event.currentTarget.value;
-//   console.log(amount);
-// })
-
-
 
 function createBoxes(amount) {
    for (let i = 1; i <= amount; i+=1) {
     const divEl = document.createElement('div');
-    console.log(divEl);
+    divEl.style.width = `${30 + i * 10}px`;
+    divEl.style.height = `${30 + i * 10}px`;
+    divEl.style.backgroundColor = getRandomHexColor();
     boxesEl.append(divEl);
     console.log(boxesEl);
    }
 }
 
 function destroyBoxes() {
-  boxesEl.reset();
+  boxesEl.innerHTML = '';
 }
 
 btnCreate.addEventListener('click', () => {
@@ -34,4 +29,7 @@ btnCreate.addEventListener('click', () => {
   console.log(amount);
   createBoxes(amount);
 });
-//btnDestroy.addEventListener('click', destroyBoxes);
+
+btnDestroy.addEventListener('click', () => {
+  destroyBoxes();
+});
