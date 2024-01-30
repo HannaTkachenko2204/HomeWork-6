e84dcea78fa54b56b27d27145a1a4c7ada917682
HannaTkachenko2204 <anna.kuvakina91@gmail.com>
2024-01-30 19:02:34 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -3,17 +3,46 @@ const formEl = document.querySelector('.login-form');
 formEl.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
+
     event.preventDefault();
-    const objectOfData = {};
-    const form = event.target;
-    const email = form.elements.email.value;
-    objectOfData.email = email;
-    const password = form.elements.password.value;
-    objectOfData.password = password;
-    console.log(objectOfData);
-    if(email === '' || password === '') {
+
+    const {elements: { email, password }} = event.target;
+
+    if(email.value === '' || password.value === '') {
         alert('All fields must be filled in!');
+    } else {
+        console.log({[email.name]:email.value, [password.name]:password.value});
     }
-    //console.log(`${email}, ${password}`);
-    form.reset();
+    
+    event.target.reset();
 }
+
+
+// function handleSubmit(event) {
+//     event.preventDefault();
+//     const form = event.target;
+//     const email = form.elements.email.value;
+//     const password = form.elements.password.value;
+//     console.log({[form.elements.email.name]:`${email}`, [form.elements.password.name]:`${password}`});
+//     if(email === '' || password === '') {
+//         alert('All fields must be filled in!');
+//     }
+//     form.reset();
+// }
+
+
+// function handleSubmit(event) {
+//     event.preventDefault();
+//     const objectOfData = {};
+//     const form = event.target;
+//     const email = form.elements.email.value;
+//     objectOfData.email = email;
+//     const password = form.elements.password.value;
+//     objectOfData.password = password;
+//     console.log(objectOfData);
+//     if(email === '' || password === '') {
+//         alert('All fields must be filled in!');
+//     }
+//     //console.log(`${email}, ${password}`);
+//     form.reset();
+// }
